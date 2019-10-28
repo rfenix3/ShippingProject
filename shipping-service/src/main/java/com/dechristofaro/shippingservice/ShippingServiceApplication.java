161a87c4dffdb55483538967ae3c90e511c7ac92
Dominick <dominick.dechristofaro@gmail.com>
2019-10-28 09:41:14 -0400

    shipping service running on port 8201 and registered with eureka

---
+++ shipping-service/src/main/java/com/dechristofaro/shippingservice/ShippingServiceApplication.java
@@ -0,0 +1,14 @@
+package com.dechristofaro.shippingservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ShippingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingServiceApplication.class, args);
+	}
+
+}
